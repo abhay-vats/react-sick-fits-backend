710d2b0f7cbfb0b39bc20c38aae83c64d2913e09
Abhay Vats <abhayvats@pm.me>
2022-10-01 17:58:51 +0530

    Setup keystone with auth

---
+++ keystone.ts
@@ -0,0 +1,35 @@
+// External dependencies
+import { createAuth } from '@keystone-next/auth';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
+import 'dotenv/config';
+
+// Local dependencies
+import { User } from './schemas/User';
+
+const databaseUrl = process.env.DATABASE_URL || 'mongodb://localhost/sick-fits';
+
+const sessionConfig = {
+  maxAge: 365 * 24 * 60 * 60,
+  secret: process.env.COOKIE_SECRET,
+};
+
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: { fields: ['name', 'email', 'password'] },
+});
+
+export default withAuth(
+  config({
+    server: { cors: { origin: [process.env.FRONTEND_URL], credentials: true } },
+    db: { adapter: 'mongoose', url: databaseUrl },
+    lists: createSchema({ User }),
+    ui: { isAccessAllowed: ({ session }) => !!session?.data },
+    session: withItemData(statelessSessions(sessionConfig), { User: 'id' }),
+  })
+);
