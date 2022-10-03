1160434aed21c0ff4b324c4fbd69b37d7bf008a4
Abhay Vats <abhayvats@pm.me>
2022-10-03 23:32:50 +0530

    Seed data configuration

--- keystone.ts
+++ keystone.ts
@@ -11,6 +11,7 @@ import 'dotenv/config';
 import { User } from './schemas/User';
 import { Product } from './schemas/Product';
 import { ProductImage } from './schemas/ProductImage';
+import { insertSeedData } from './seed-data';
 
 const databaseUrl = process.env.DATABASE_URL || 'mongodb://localhost/sick-fits';
 
@@ -28,10 +29,30 @@ const { withAuth } = createAuth({
 
 export default withAuth(
   config({
-    server: { cors: { origin: [process.env.FRONTEND_URL], credentials: true } },
-    db: { adapter: 'mongoose', url: databaseUrl },
-    lists: createSchema({ User, Product, ProductImage }),
-    ui: { isAccessAllowed: ({ session }) => !!session?.data },
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databaseUrl,
+      async onConnect(keystone) {
+        console.log('Connected to the database');
+        if (process.argv.includes('--seed-data')) {
+          await insertSeedData(keystone);
+        }
+      },
+    },
+    lists: createSchema({
+      User,
+      Product,
+      ProductImage,
+    }),
+    ui: {
+      isAccessAllowed: ({ session }) => !!session?.data,
+    },
     session: withItemData(statelessSessions(sessionConfig), { User: 'id' }),
   })
 );
